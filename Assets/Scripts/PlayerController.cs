70108b99ac155d90d6a71278aa6ed4bde9102fe1
Christina Piete <christina.piete@rub.de>
2020-10-04 16:20:31 +0200

    animations added, still buggy as hell

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,14 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 20.0f;
     public Recorder cameraInHand;
+    public Animator anim;
+    Camera cam;
+    Vector3 camPos = new Vector3(0, 0, -10);
+    float vertical = 0f;
+    float horizontal = 0f;
+    [SerializeField] float viewField = 8f;
+    bool moving = false;
+    public Vector2 movingDirection = Vector2.zero;
     public Projector projectorPrefab;
     private int _lookDirection;
     public int LookDirection { 
@@ -15,11 +23,58 @@ public class PlayerController : MonoBehaviour
         UpdateDirection();}
     }
 
+    //methoden für animationen
+    #region methods for animations
+    void SetFalse()
+    {
+        StandStill();
+        vertical = 0f;
+        horizontal = 0f;
+        movingDirection = Vector2.zero;
+    }
 
+    void SetIdlesFalse()
+    {
+        anim.SetBool("idle", false);
+        anim.SetBool("idleLeft", false);
+        anim.SetBool("idleRight", false);
+        anim.SetBool("idleUp", false);
+    }
+
+    public void StandStill()
+    {
+        moving = false;
+        if (vertical > 0)
+        {
+            anim.SetBool("idleUp", true);
+            vertical = 0f;
+        }
+        else if (vertical < 0)
+        {
+            anim.SetBool("idle", true);
+            vertical = 0f;
+        }
+
+        if (horizontal > 0)
+        {
+            anim.SetBool("idleRight", true);
+            horizontal = 0f;
+        }
+
+        else if (horizontal < 0)
+        {
+            anim.SetBool("idleLeft", true);
+            horizontal = 0f;
+        }
+    }
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        anim.SetBool("idle", true);
+        cam = Camera.main;
     }
     public new Rigidbody2D rigidbody2D;
 
@@ -46,8 +101,19 @@ public class PlayerController : MonoBehaviour
             x += 1;
         velocity = new Vector2(x, y).normalized * speed;
 
+        //determine if moving and direction
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+
+        if (Input.GetButton("Vertical") || Input.GetButton("Horizontal"))
+            moving = true;
+        else
+        {
+            StandStill();
+        }
+
         // Find out look direction
-        if((cameraInHand?.recordingstatus ?? Recorder.Recordingstatus.NO_RECORDING) == Recorder.Recordingstatus.NO_RECORDING) {
+        if ((cameraInHand?.recordingstatus ?? Recorder.Recordingstatus.NO_RECORDING) == Recorder.Recordingstatus.NO_RECORDING) {
             var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePos = transform.worldToLocalMatrix.MultiplyPoint(mousePos);
             if(mousePos.x > mousePos.y) {
@@ -87,9 +153,38 @@ public class PlayerController : MonoBehaviour
     private void UpdateDirection()
     {
         // TODO: Update Sprite
+        switch (_lookDirection)
+        {
+            case 0: // Up
+                SetIdlesFalse();
+                anim.SetFloat("walkVertical", vertical);
+                anim.SetFloat("walkHorizontal", 0f);
+                break;
+            case 1: // Right
+                SetIdlesFalse();
+                anim.SetFloat("walkHorizontal", horizontal);
+                anim.SetFloat("walkVertical", 0f);
+                break;
+            case 2: // Down
+                SetIdlesFalse();
+                anim.SetFloat("walkVertical", vertical);
+                anim.SetFloat("walkHorizontal", 0f);
+                break;
+            default: // Left
+                SetIdlesFalse();
+                anim.SetFloat("walkHorizontal", horizontal);
+                anim.SetFloat("walkVertical", 0f);
+                break;
+        }
+        if (horizontal == 0 && vertical == 0)
+        {
+            anim.SetFloat("walkHorizontal", 0f);
+            anim.SetFloat("walkVertical", 0f);
+        }
+        movingDirection = new Vector2(horizontal, vertical);
 
         // set camera direction
-        switch(_lookDirection) {
+        switch (_lookDirection) {
             case 0: // Up
                 if(cameraInHand != null)
                     cameraInHand.transform.localPosition = new Vector3(0,0.326f,-0.017f);
@@ -111,5 +206,12 @@ public class PlayerController : MonoBehaviour
                     cameraInHand.transform.rotation = new Quaternion(0,0,1,0);
                 break;
         }
+        SetFalse();
+    }
+
+    private void LateUpdate()
+    {
+        cam.transform.position = transform.position + camPos;
+        cam.orthographicSize = viewField;
     }
 }
