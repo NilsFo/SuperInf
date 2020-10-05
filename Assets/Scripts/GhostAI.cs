007a0c50e5ed7468c3f494b684b4b08f11d4133e
PatrickQ <Patrick@Quiskamp.de>
2020-10-05 16:06:08 +0200

    Cleanup to level 4

--- Assets/Scripts/GhostAI.cs
+++ Assets/Scripts/GhostAI.cs
@@ -8,12 +8,15 @@ public class GhostAI : MonoBehaviour
 {
     public float killDistance = 10f;
     public UnityEvent onKillEvent;
+
+    private GameObject _player;
     
-    private void OnTriggerEnter2D(Collider2D other)
+    
+    private void FixedUpdate()
     {
-        if (other.gameObject.tag == "Player")
+        if (_player)
         {
-            Vector3 otherPos3d = other.gameObject.transform.position;
+            Vector3 otherPos3d = _player.transform.position;
             Vector3 myPos3d = transform.position;
             Vector2 otherPos = new Vector2(otherPos3d.x, otherPos3d.y);
             Vector2 myPos = new Vector2(myPos3d.x, myPos3d.y);
@@ -23,6 +26,22 @@ public class GhostAI : MonoBehaviour
             {
                 onKillEvent.Invoke();
             }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _player = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _player = null;
         }
     }
 }
